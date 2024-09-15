906de90abdd8442f1c2e99440b611e8ca3cabff3
raghavendra111222 <dosiraghavendra@gmail.com>
2024-09-16 00:34:06 +0530

    Add files via upload

---
+++ cement-strength-prediction-project (1)/cement-strength-prediction-project/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="raghavendra",
+    author_email="dosiraghavendra@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
+
